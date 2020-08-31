86502337cf95e466cc85745765d106d809d297de
Jonatan <jonatankwins@gmail.com>
2020-08-31 17:19:00 +0200

    excercice 2 - test for float number

--- test_calculator.py
+++ test_calculator.py
@@ -2,3 +2,6 @@ import calculator
 
 def test_add_integers_excercice_1():
     assert calculator.add(1,2) == 3
+
+def test_add_float_excercice_2():
+    assert round(calculator.add(0.1,0.2),1) == 0.3
