f05fd28263acd202defd32787fbc731fe16769fc
Jonatan <jonatankwins@gmail.com>
2020-08-31 17:21:47 +0200

    excercice 3 - test for string concatenation

--- test_calculator.py
+++ test_calculator.py
@@ -5,3 +5,6 @@ def test_add_integers_excercice_1():
 
 def test_add_float_excercice_2():
     assert round(calculator.add(0.1,0.2),1) == 0.3
+
+def test_add_string_excercice_3():
+    assert 'IN1910' == calculator.add('IN','1910')
