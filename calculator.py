d58db5a590e26d42690835ca47805dcf231af1d4
Jonatan <jonatankwins@gmail.com>
2020-08-31 19:02:01 +0200

    excercice 4 but sin taylor series expansion does not work

--- calculator.py
+++ calculator.py
@@ -1,5 +1,28 @@
 def add(x,y):
     return x+y
-        
 
+def factorial(x):
+    if x != 0:
+        return x*factorial(x-1)
+    else:
+        return 1
 
+def sin(x,N):
+    sum = 0
+    for n in range(0,N+1):
+        sum += (((-1)^n)*x^(2*n+1))/factorial(2*n+1)
+    return sum
+
+def divide(x,y):
+    return x/y
+
+def double_factorial(x):
+    if x > 1:
+        return x*double_factorial(x-2)
+    else:
+        return 1
+
+def absolute_value(x):
+    if x < 0:
+        x = -x
+    return x
--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,5 @@
 import calculator
+import math
 
 def test_add_integers_excercice_1():
     assert calculator.add(1,2) == 3
@@ -7,4 +8,19 @@ def test_add_float_excercice_2():
     assert round(calculator.add(0.1,0.2),1) == 0.3
 
 def test_add_string_excercice_3():
-    assert 'IN1910' == calculator.add('IN','1910')
+    assert calculator.add('IN','1910') == 'IN1910'
+
+def test_factorial_excercice_4():
+    assert calculator.factorial(5) == math.factorial(5)
+
+def test_sin_excercice_4():
+    assert calculator.sin(0,0) == 0
+
+def test_divide_excercice_4():
+    assert calculator.divide(6,3) == 2
+
+def test_double_factorial_excercice_4():
+    assert calculator.double_factorial(8) == 384
+
+def test_absolute_value_excercice_4():
+    assert calculator.absolute_value(-5) == calculator.absolute_value(5)
