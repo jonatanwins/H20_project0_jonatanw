0a304349d71a5e6a66947679745b2b0cd261078d
Jonatan <jonatankwins@gmail.com>
2020-09-02 11:13:53 +0200

    deleted pycache and pytest cache

--- test_calculator.py
+++ test_calculator.py
@@ -2,15 +2,19 @@ import calculator
 import math
 import pytest
 
+# Excercice 1
 def test_add_integers_excercice_1():
     assert calculator.add(1,2) == 3
 
+# Excercice 2
 def test_add_float_excercice_2():
     assert math.isclose(calculator.add(0.1,0.2),0.3, rel_tol=0.0001)
 
+# Excercice 3
 def test_add_string_excercice_3():
     assert calculator.add('IN','1910') == 'IN1910'
 
+# Excercice 4
 def test_factorial_excercice_4():
     assert calculator.factorial(5) == math.factorial(5)
 
@@ -26,6 +30,7 @@ def test_double_factorial_excercice_4():
 def test_absolute_value_excercice_4():
     assert calculator.absolute_value(-5) == calculator.absolute_value(5)
 
+# Excercice 5
 def test_add_excercice_5():
     with pytest.raises(TypeError):
         calculator.add('4',2)
@@ -35,10 +40,12 @@ def test_divide_by_zero_excercice_5():
         calculator.divide(1,0)
 
 # Excercice 6
-@pytest.mark.parametrize(
-    "arg, expected_output", [[(-1,1),0],[(0.4,0.6),1.0],[('ananas','biter'),'ananasbiter']]
-)
-def test_add_excercice_6(arg,expected_output):
+@pytest.mark.parametrize("arg, expected_output", [[(-1,1),0],[('ananas','biter'),'ananasbiter']])
+def test_add_integer_string_excercice_6(arg,expected_output):
+    assert calculator.add(arg[0], arg[1]) == expected_output
+
+@pytest.mark.parametrize("arg, expected_output", [[(-1.0,1.0),0.0],[(0.4,0.6),1.0]])
+def test_add_float_excercice_6(arg,expected_output):
     assert calculator.add(arg[0], arg[1]) == expected_output
 
 @pytest.mark.parametrize("x", [0,5,10])
